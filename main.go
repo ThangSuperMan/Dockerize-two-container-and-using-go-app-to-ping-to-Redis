a2184e4c276426989f094091a004248a3212b6de
thangsuperman <thangjenny2002@gmail.com>
2022-12-31 22:46:15 +0700

    First pure commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"log"
+)
+
+func haltOn(err error) {
+	if err != nil {
+		log.Fatal("Error here: ", err)
+	}
+}
+
+func main() {
+	fmt.Println("hello")
+	client := redis.NewClient(&redis.Options{
+		//container name:port
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	haltOn(err)
+	fmt.Println("Pong :>> ", pong)
+}
